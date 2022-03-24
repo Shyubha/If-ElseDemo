a608cad9365dd699d33f617f1f6fdf2ece1f5c91
Shyubha <86038837+Shyubha@users.noreply.github.com>
2022-03-24 11:13:14 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TSystems_IfElse_UserInput
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        start:
+            Console.WriteLine("Please Enter Any NUMBER");
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            if (num % 2 == 0)
+            {
+                Console.WriteLine("Number is EVEN");
+            }
+            else
+            {
+                Console.WriteLine("Number is ODD");
+            }
+            Console.ReadLine();
+            goto start;
+        }
+    }
+}
